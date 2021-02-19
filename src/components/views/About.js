9fad34ddc884177b7826f6375668f879b1174b2f
chrisean550 <czn1@pitt.edu>
2021-02-19 17:46:31 -0500

    New images and fix bio format

--- src/components/views/About.js
+++ src/components/views/About.js
@@ -21,12 +21,12 @@ export default function About() {
                         <img src={headshot} className="main-img" alt="" />
                     </div>
                     <div className="slide-right about-bio-container">
-                        <p className="md-text">
+                        <p className="sm-text">
                         Hi! My name is Hannah. I grew up in Atlanta, GA but have been calling Pittsburgh, PA my home for the last 3 years after deciding to pursue my college career at the University of Pittsburgh. I am a self-taught artist and mainly work with acrylics and charcoals. More recently, I’ve taken a dive into the world of ‘fluid acrylics’ and have been loving experimenting with various pouring mediums/techniques.
-Art is where I feel the most comfortable to express who I am and to let go of any stress or anxiety I am feeling in the moment. When I start a piece, I disconnect from the world and immerse myself into the body of work. Although I prefer to leave the interpretation of my artwork up to the viewer, I do tend to see a lot of my own emotion poured out onto the piece.
-That being said, my art is an extension of myself. 
-
-                            
+                        <br/><br/>
+                        Art is where I feel the most comfortable to express who I am and to let go of any stress or anxiety I am feeling in the moment. When I start a piece, I disconnect from the world and immerse myself into the body of work. Although I prefer to leave the interpretation of my artwork up to the viewer, I do tend to see a lot of my own emotion poured out onto the piece.
+                        <br/><br/>
+                        That being said, my art is an extension of myself. 
                         </p>
                     </div>
                 </div>
